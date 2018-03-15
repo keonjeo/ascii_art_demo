7486f98805c130b4801756f8fede1d2a9fcfb7bd
Keon Ye <jnuyeyizhou@gmail.com>
2018-03-16 00:05:47 +0800

    add fire dragon ascii art

---
+++ fire_dragon.sh
@@ -0,0 +1,55 @@
+#!/bin/bash --login
+
+clear
+cat << "EOF"
+                        Ko0
+                      NolccdkkkkK
+                      WxccccccccckXWWNW
+                   XKKoccccccclxxxxOKKKN
+                 WOcccccclxxxxx0KKKKKKXNW
+                 KcccccclxKKKKKKKKKKKKNW
+                 XxxccccoOK00KKKXNXNNW
+                   XxcccccccxKKKW
+                    WKKKxcccckKKXXW
+                        0lcccdO0KKW
+                         WKccccdKXW          W
+                          N0K0kdo0W      000Oxk0W
+                                NkkXW   W;,,,;kOOW
+                    WXKX          NkOW  Kk.  ''oOOW                 NNW
+                  X0OOKW            X0XW O.    .xOOW               KKXkW
+               WXKKOxX                X0WN,.    :kcx0N              X0OkW
+             K0kkkkOOkO                WW c.    .dd0kkW              X0Ok0KXN
+            KKOdlcclkOkK              NK0Oc.     ckxkkW              KkxxOOOkX
+          NKKkc;,'',,xOkW           0kdkxcl:.    lOOOkN             XOOOollokkX
+         KKKo:::;'',,ckOO           NKNXO;,cloc,xOOkdOkX           WOOk:,,,,;dkK
+      WX0KOl:::::,.',:lkkN            Wd,'',:oxOOkdodkxkX          0Kx,',;,,,':dkK
+      00OOl::::::;'',::oO0             OolloddoooolK  NOxN        X0x,,',:;,,,,,cxkKW
+      OOOo;:::::::,',::cxxW            WXXKocloodOd     N        XKO;;,,':::;;,,,,cxk0W
+      kOo;::::::::,,,:::okONN              kllldOOk            W000:::,''::::::;;;,,ckkK
+     0kd::::::;;::,;,:::lOKXXKX            0kddOOOk           NO0Ol::::'';:::::::::;;;xkW
+    Kxdc::::::,,::,:,::::oxKKKKKX          0OOOOOOk         N0kOOl;::::;',:::::::::::;,dkW
+   Kkdc::::::;,,;:,,dxcdxl::cok00KN        0OOOOOOk  0O0XXKOkOOd:;::::::,,:::::::::::::;dO
+  Nkxc:::::::;,,;:;;kkOcxkO::;,:lxO0KK00N XOOOOOOOxKxodOkxxxol;,;:::::::;;,::::c::::::::ckOW
+ NOkl::::::::,,,,:OddkkOkOOl::;,,,:dOOOOk0KkkOOOOOOoloo:,,,',,,;:::;;:::,;,:cccc::::::::;oOOW
+N0Xdc:::::::;,,,,:ldkkxOOOx;::::;,,:;kkk00kkxxkkOOOOd:,,,,,,;::::c;x0o;:,;,::ccckko:::::::kOOW
+N0Ol::::::::,':ld::;;loxOOOo:::::o0KkdOOOOOOOOOOOOOOOkxOk;,;::::kKxxkllc,;;;:cck0K0:::::::oOO0
+KOxc::::::;lkX   No;,::lxOOOdclokK0OOOOOOOOOOOOOOOOOOOkOOkc::Ox::Okkkx0Oc::ccclOKKd:::::::coxO
+0Od::::;lON        O:;:;:xOOOkxddxxOOOOkO00000OOkOOOOOOOkxxOON 0lOdkOkkx:::ccckKKKo:::::::cdk0
+OOo::;lK            Nxc::xkxxkO000dOOk0XNNNNNNNNX0OkOOOOxOkk000OOOOxOXWXdccccl0K0xlldd:;:::oxX
+kko:;kW               XkdW W     KkkkXNNNNNNNNNNNNNKkkOOO0 KkOOOOO0N  OcdkOdlOKKdlllN W0c;:lxW
+Oxd;k                 NXXXXXXXKXKkkOXNNNNNNNNNNNNNNNNOkOOk  W00KN     kc0KK00KKK00OldW  Wx;lk
+Xddl               NXK000000OkOOdkkNNNNNNNNNNNNNNNNNNNkOOxKN          WoxKK0OKKKKKKOX     koO
+ OxO            WXKXNKOxkkkk0KXOxxKNNNNNNNNNNNNNNNNNNNOkOxOO0X         NKXOkkKKKKW        KxO
+ W0K          WK0KXNNNNXKO0XNNXOxdXXKKKKKXXXNNNNNNNNNNOkOOOOOkOX        WOOOkWWNW         X0X
+             XOOOOOO000kOXNNNNKOOxK000000000KKXXNNNNNNkkxOOOOOOdkKXXXNX0OOOkW
+            0xOOOOOOOOkKNNNNXK0OOdO0000000000000KKKKK0llOOOOOOOOxk0K00kxxkkN
+            KOkOOOOOOkNNKK00OOOkdooO00000000000000000llkOOOOOOOOkoxkxxdoxKW
+             0OkxxxxdoXKOOOOOOkoooolx00000000000000kllxOOOOOOkxoolldddkKW
+              00kxdddoOKOOkxdolllloxkkOO0000000O0xolookOkxxdoooolo0KXN
+               K00O0000OkOOOkxdllod000K0OOKXXNNW WOoolllllllloooOW
+                  NXXKkxkOOOOOOkoolOKXNN           xxkkdoooooldW
+                    NXKkxkOOkOOkoldN              W0OkOOkxkOOxxXXXW
+ddddddddddddddddddddcllc,col;loNNxN                  KXWkxx0KXKKXXK
+lldooododolldoodoooooooccllllKNOKW                    NKN   WXXKKWW
+lllllllllllllllllllllll:cllllK                                 WW
+EOF
